d1dce027bab896cd39ae43257ab9f0bf0171d165
Bryce Klinker <bklinker@pillartechnology.com>
2018-06-24 22:38:09 -0500

    Working unit test validating arrival time is after 5:00 PM

--- babysitter/src/BabySitter.Core/NightlyChargeCalculator.cs
+++ babysitter/src/BabySitter.Core/NightlyChargeCalculator.cs
@@ -1,10 +1,20 @@
-﻿namespace BabySitter.Core
+﻿using System;
+using NodaTime;
+
+namespace BabySitter.Core
 {
     public class NightlyChargeCalculator
     {
         public int Calculate(NightlyChargeParameters parameters)
         {
+            if (IsArrivalTimeInvalid(parameters.ArrivalTime))
+                throw new InvalidOperationException();
             throw new System.NotImplementedException();
+        }
+
+        private bool IsArrivalTimeInvalid(LocalTime arrivalTime)
+        {
+            return arrivalTime < new LocalTime(17, 0);
         }
     }
 }
