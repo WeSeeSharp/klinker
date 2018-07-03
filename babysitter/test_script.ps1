1fe6d1efaa264daf7514ae969170e029b77d21f7
Bryce Klinker <bklinker@pillartechnology.com>
2018-07-02 22:34:21 -0500

    More test script tweaks

--- babysitter/test_script.ps1
+++ babysitter/test_script.ps1
@@ -1,8 +1,11 @@
 pushd .\babysitter
 
 $hasErrors = $false
-$testDirectories = Get-ChildItem -Path .\test\* -Directory -Include *.Test,*.Specs
+$testDirectories = Get-ChildItem -Path .\test\* -Directory
 foreach($folder in $testDirectories) {
+    if ($folder.FullName -notcontains "Test" -or $folder.FullName -notcontains "Specs")
+        continue
+    
     dotnet test $folder.FullName
     
     if ($hasErrors -or $lastExitCode -ne 0) {
