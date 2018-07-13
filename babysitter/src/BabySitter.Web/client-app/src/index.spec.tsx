a86be35f1fca39da3bb197e99d743541007d1a91
Bryce Klinker <bklinker@pillartechnology.com>
2018-07-12 22:30:18 -0500

    Setup jest and prettier

---
+++ babysitter/src/BabySitter.Web/client-app/src/index.spec.tsx
@@ -0,0 +1,3 @@
+describe('index', () => {
+  it('should render root', () => {});
+});
--- babysitter/src/BabySitter.Web/client-app/webpack.make.js
+++ babysitter/src/BabySitter.Web/client-app/webpack.make.js
@@ -1,70 +1,57 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CheckerPlugin } = require("awesome-typescript-loader");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
 
 module.exports = function(env) {
   return {
-    mode: isDev(env) ? "development" : "production",
-    devtool: isDev(env) ? "source-map" : "cheap-module-source-map",
+    mode: isDev(env) ? 'development' : 'production',
+    devtool: isDev(env) ? 'source-map' : 'cheap-module-source-map',
     entry: {
-      main: path.resolve(__dirname, "src", "index.tsx")
+      main: path.resolve(__dirname, 'src', 'index.tsx'),
     },
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: isDev(env) ? "[name].js" : "[name].min.js"
+      path: path.resolve(__dirname, 'dist'),
+      filename: isDev(env) ? '[name].js' : '[name].min.js',
     },
     resolve: {
-      extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
           exclude: /node_modules/,
-          use: [
-            {
-              loader: "babel-loader"
-            },
-            {
-              loader: "awesome-typescript-loader",
-              options: {
-                useBabel: true,
-                babelCore: "@babel/core",
-                useCache: true,
-                cacheDirectory: path.resolve(__dirname, ".cache")
-              }
-            }
-          ]
+          use: 'babel-loader',
         },
         {
           test: /\.scss$/,
           use: [
-            { loader: "style-loader", options: { sourceMap: true } },
+            { loader: 'style-loader', options: { sourceMap: true } },
             {
-              loader: "css-loader",
-              options: { sourceMap: true, modules: true, importLoaders: 1 }
+              loader: 'css-loader',
+              options: { sourceMap: true, modules: true, importLoaders: 1 },
             },
-            { loader: "postcss-loader", options: { sourceMap: true } },
-            { loader: "sass-loader", options: { sourceMap: true } }
-          ]
-        }
-      ]
+            { loader: 'postcss-loader', options: { sourceMap: true } },
+            { loader: 'sass-loader', options: { sourceMap: true } },
+          ],
+        },
+      ],
     },
     plugins: [...(isDev(env) ? getDevPlugins(env) : getProductionPlugins(env))],
     devServer: {
-      contentBase: path.resolve(__dirname, "dist"),
+      contentBase: path.resolve(__dirname, 'dist'),
       compress: true,
       historyApiFallback: true,
       hot: true,
       inline: true,
-      open: true
-    }
+      open: true,
+    },
   };
 };
 
 function isDev(env) {
-  return env === "dev";
+  return env === 'dev';
 }
 
 function getDevPlugins(env) {
@@ -75,8 +62,8 @@ function getProductionPlugins(env) {
   return [
     ...getBasePlugins(env),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true
-    })
+      sourceMap: true,
+    }),
   ];
 }
 
@@ -84,13 +71,13 @@ function getBasePlugins(env) {
   return [
     new CheckerPlugin(),
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify(env)
-      }
+      'process.env': {
+        NODE_ENV: JSON.stringify(env),
+      },
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
-      inject: "body"
-    })
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      inject: 'body',
+    }),
   ];
 }
